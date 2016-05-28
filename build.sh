591638ba417cb9da39c762b86de0828404b7257b
Matt Todd <matt.c.todd@gmail.com>
2016-05-27 23:01:03 -0500

    finally got everything logging

--- build.sh
+++ build.sh
@@ -11,13 +11,18 @@ docker build -t caspian311/github-scorer-app:1.0 app_server
 docker build -t caspian311/github-scorer-web:1.0 web_server
 
 echo 'create data directories'
-data_dir=$(mkdir -p data ; cd data &> /dev/null; pwd; cd - &> /dev/null)
-log_dir=$(mkdir -p log ; cd log &> /dev/null; pwd; cd - &> /dev/null)
+mkdir -p data
+mkdir -p logging
+
+data_dir=$(cd data &> /dev/null; pwd; cd - &> /dev/null)
+db_log_dir=$(mkdir -p logging/db ; cd logging/db &> /dev/null; pwd; cd - &> /dev/null)
+app_log_dir=$(mkdir -p logging/app ; cd logging/app &> /dev/null; pwd; cd - &> /dev/null)
+web_log_dir=$(mkdir -p logging/web ; cd logging/web &> /dev/null; pwd; cd - &> /dev/null)
 
 echo 'run-images'
-docker run -d -v $data_dir:/var/lib/mysql -p 3306:3306 caspian311/github-scorer-db:1.0
-docker run -d -v $log_dir:/usr/src/app/log -p 3000:3000 caspian311/github-scorer-app:1.0
-docker run -d -p 80:80 caspian311/github-scorer-web:1.0
+docker run -d -v $data_dir:/var/lib/mysql -v $db_log_dir:/var/log/mysql -p 3306:3306 caspian311/github-scorer-db:1.0
+docker run -d -v $app_log_dir:/usr/src/app/log -v $app_log_dir:/usr/src/app/log -p 3000:3000 caspian311/github-scorer-app:1.0
+docker run -d -v $web_log_dir:/var/log/nginx -p 80:80 caspian311/github-scorer-web:1.0
 
 docker ps
 
