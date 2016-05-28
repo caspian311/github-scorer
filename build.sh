7bc738c0ae5109620e55100f3aca263afc9ceab0
Matt Todd <matt.c.todd@gmail.com>
2016-05-27 22:51:04 -0500

    got the data actually mounting now

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ docker build -t caspian311/github-scorer-app:1.0 app_server
 docker build -t caspian311/github-scorer-web:1.0 web_server
 
 echo 'create data directories'
-data_dir=$(mkdir -p data ; cd data &> /dev/null; echo pwd; cd - &> /dev/null)
-log_dir=$(mkdir -p log ; cd log &> /dev/null; echo pwd; cd - &> /dev/null)
+data_dir=$(mkdir -p data ; cd data &> /dev/null; pwd; cd - &> /dev/null)
+log_dir=$(mkdir -p log ; cd log &> /dev/null; pwd; cd - &> /dev/null)
 
 echo 'run-images'
 docker run -d -v $data_dir:/var/lib/mysql -p 3306:3306 caspian311/github-scorer-db:1.0
