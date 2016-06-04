c79a44c638ea675d449d5d64a0571639b441c77c
Matt Todd <matt.c.todd@gmail.com>
2016-06-04 07:18:00 -0500

    formatting niceties

--- app_server/app/controllers/api/scores_controller.rb
+++ app_server/app/controllers/api/scores_controller.rb
@@ -2,8 +2,8 @@ class Api::ScoresController < ApplicationController
   def show
     results = []
     params[:users].each do |user|
-      results << { user => GithubScoreHandler.new(user).handle }
+      results << { username: user, score: GithubScoreHandler.new(user).handle }
     end
-    render json: results
+    render json: results.sort { |a, b| b[:score] <=> a[:score] }
   end
 end
